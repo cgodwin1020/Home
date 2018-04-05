546da6e65504487d9854bb1432f4811883b9b68d
cgodwin1020 <cgodwin1020@gmail.com>
2018-04-05 09:23:35 -0400

    Add files via upload

---
+++ exgauss.R
@@ -0,0 +1,31 @@
+
+#package retimes
+
+exgauss_list <- list()
+
+by(mydata_skewness, list(mydata_skewness$promptRESP_RFA, mydata_skewness$subject_ID), function(x){ i <- as.character(x$promptRESP_RFA)
+cond <- i[1]
+s <- as.character(x$subject_ID)
+sub <- s[1]
+plot_filename <- sprintf("exgauss_%s_condition_%s.png", sub, cond)
+png(plot_filename)
+exgauss <- timefit(x$RTT, iter = 1000, plot = TRUE)
+dev.off()
+data_filename <- sprintf("ex_gauss_%s_condition_%s.RData", sub, cond)
+save(exgauss, file = data_filename)
+mu_value <- exgauss@par[1]
+sigma_value <- exgauss@par[2]
+tau_value <- exgauss@par[3]
+logLik <- exgauss@logLik
+AIC <- exgauss@AIC
+BIC <- exgauss@BIC
+exgauss_df <- c(subject_name = sub, condition_type = cond, mu_value = mu_value, sigma_value = sigma_value, tau_value = tau_value, logLik = logLik, AIC = AIC, BIC = BIC)
+current_list <- list(exgauss_df)
+exgauss_list <<- append(exgauss_list, current_list)
+})
+
+
+
+
+
+
---
+++ skewness_syntax.R
@@ -0,0 +1,32 @@
+
+
+#load mydata_skewness with 2206 obs of 3 variables
+
+#d = vector of RTT values from each condition per subject
+#calculating standard error of skewness as in SPSS
+skew_se_function <- function(d) {
+  n <- length(d)
+  g1 <- sqrt((6*(n-2))/((n+1)*(n+3)))
+  skew_g1 <- ((sqrt(n*(n-1)))/(n-2)) * g1
+  return(skew_g1)
+  
+}
+
+
+skewness_list <- list()
+  
+
+by(sd, list(sd$promptRESP_RFA, sd$subject_ID), function(x){ skew_type2 <- skewness(x$RTT, type = 2)
+  skew_se <- skew_se_function(x$RTT)
+  i <- as.character(x$promptRESP_RFA)
+  condition <- i[1]
+  s <- as.character(x$subject_ID)
+  subject <- s[1]
+  skewness_df <- c(subject_name = subject, condition_type = condition, skew_type2_value = skew_type2, skew_se_value = skew_se)
+  current_df <- skewness_df
+  current_list <- list(current_df)
+  skewness_list <<- append(skewness_list, current_list)
+})
+
+
+
